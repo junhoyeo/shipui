017e517599344aff3ef132ff64f363f5d5d0b4bd
Junho Yeo <i@junho.io>
2023-04-04 03:33:51 +0900

    [shipui] Export types for `Tab`

--- packages/shipui/src/index.ts
+++ packages/shipui/src/index.ts
@@ -17,7 +17,7 @@ export { Paragraph } from '@/components/Paragraph'
 export { Profile } from '@/components/Profile'
 export { Row } from '@/components/Row'
 export { Select, type SelectOption, type SelectProps } from '@/components/Select'
-export { Tab } from '@/components/Tab'
+export { Tab, type TabProps, type TabOption } from '@/components/Tab'
 export { Textarea } from '@/components/Textarea'
 export {
   createPalette,
