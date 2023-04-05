4f95b95582b74860296b2d497e96aa2ce2cb660c
Junho Yeo <i@junho.io>
2023-04-06 00:16:38 +0900

    [shipui] Make `Box` inherit `Col`

--- packages/shipui/src/components/Box.tsx
+++ packages/shipui/src/components/Box.tsx
@@ -2,15 +2,13 @@ import styled from '@emotion/styled'
 
 import { DefaultColors } from '@/utils/colors'
 
-export const Box = styled.div`
+import { Col } from './Col'
+
+export const Box = styled(Col)`
   padding: 20px;
   width: 100%;
 
   border-radius: 20px;
   background-color: ${DefaultColors.foreground};
-
-  display: flex;
-  flex-direction: column;
-  gap: 8px;
   box-shadow: 0px 4px 16px rgba(0, 0, 0, 0.04);
 `
