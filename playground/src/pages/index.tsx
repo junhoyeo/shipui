ac1920c993994fe87fe02996b15909df93124864
Junho Yeo <i@junho.io>
2023-05-22 20:34:10 +0900

    [playground] Add Jazzicon generator draft

--- playground/src/pages/index.tsx
+++ playground/src/pages/index.tsx
@@ -1,11 +1,24 @@
 import { NextPage } from 'next'
-import { Badge, Box, Button, Input, PageContainer, Select, SelectOption } from 'shipui'
+import { Badge, Box, Button, Input, Jazzicon, PageContainer, Select, SelectOption } from 'shipui'
 
 const options: SelectOption[] = [
   { value: 'mango', label: '🥭 Mango' },
   { value: 'peach', label: '🍑 Peach' },
   { value: 'apple', label: '🍎 Apple' },
   { value: 'banana', label: '🍌 Banana' },
+]
+
+const colors = [
+  '#1E8CE0',
+  '#92D0FF',
+  '#C7D3F7',
+  '#C4E0FE',
+  '#ABF2E1',
+  '#F9F7A2',
+  '#FFE1AC',
+  '#FFC537',
+  '#FAB1F0',
+  '#FF9088',
 ]
 
 const HomePage: NextPage = () => {
@@ -15,7 +28,13 @@ const HomePage: NextPage = () => {
         <Badge>Badge</Badge>
         <Input />
         <Select options={options} onChange={(value) => console.log(value)} defaultValue="mango" />
-        <Button>Button</Button>
+
+        <Jazzicon address="1324545" colors={colors} />
+        <Jazzicon address="45" colors={colors} />
+        <Jazzicon address="5645567467yetgrgggert" colors={colors} />
+        <Jazzicon address="fergrgf434334gerrg" colors={colors} />
+        <Jazzicon address="1324545" colors={colors} />
+        <Button>Generate</Button>
       </Box>
     </PageContainer>
   )
